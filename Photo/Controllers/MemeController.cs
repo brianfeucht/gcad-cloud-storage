1371e552cbf34ab0521fa993388984d776b7b77d
Brian Feucht <brian@litmus.com>
2015-06-01 21:00:29 -0700

    Basic MVC forms

---
+++ Photo/Controllers/MemeController.cs
@@ -0,0 +1,24 @@
+﻿using Photo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Photo.Controllers
+{
+    public class MemeController : Controller
+    {
+        // GET: Meme
+        public ActionResult Index(Guid Id)
+        {
+            return View();
+        }
+
+        // POST: Meme
+        public ActionResult Create(Meme meme, HttpPostedFileBase upload)
+        {
+            return View();
+        }
+    }
+}
---
+++ Photo/Models/Meme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using static System.Net.WebRequestMethods;
+
+namespace Photo.Models
+{
+    public class Meme
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Meme Text")]
+        public string Text { get; set; }
+    }
+}
