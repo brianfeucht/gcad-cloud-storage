aa71d4c2acdc55a168404bac2f6de00b8c01cce6
Brian Feucht <brian@litmus.com>
2015-06-10 21:27:43 -0700

    Azure Queue Implementation Failing Unit Tests

---
+++ Photo.Azure/AzureQueue.cs
@@ -0,0 +1,33 @@
+﻿using Photo.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photo.Core.Models;
+
+namespace Photo.Azure
+{
+    public class AzureQueue : ICloudQueue
+    {
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event EventHandler NewMessageArrived;
+
+        public Task<MemeRequest> DequeueMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EnqueueMessage(MemeRequest message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Photo.UnitTest/Azure/AzureQueueFixture.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Photo.Azure;
+using Photo.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.UnitTest.Azure
+{
+    [TestFixture]
+    public class AzureQueueFixture
+    {
+        [Test]
+        public async Task Enqueue_EnqueuesMessage()
+        {
+            var queue = new AzureQueue();
+            await queue.EnqueueMessage(new MemeRequest());
+
+            Assert.AreEqual(1, queue.Count);
+        }
+
+        [Test]
+        public async Task Enqueue_NotifiesOfNewMessage()
+        {
+            var wasNotified = false;
+            var queue = new AzureQueue();
+            queue.NewMessageArrived += (s, e) => { wasNotified = true; };
+
+            await queue.EnqueueMessage(new MemeRequest());
+
+            Assert.IsTrue(wasNotified);
+        }
+
+        [Test]
+        public async Task Enqueue_DequeuesMessage()
+        {
+            var localQueue = new AzureQueue();
+            var expected = new MemeRequest();
+            await localQueue.EnqueueMessage(expected);
+            await localQueue.EnqueueMessage(new MemeRequest());
+
+            var actual = await localQueue.DequeueMessage();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
