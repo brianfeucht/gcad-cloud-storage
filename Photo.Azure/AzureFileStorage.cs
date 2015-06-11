3feffb6071bf9a443258bb19a912a5036e2ec504
Brian Feucht <brian@litmus.com>
2015-06-10 20:11:05 -0700

    Implement failing unit tests for AzureFileStorage

---
+++ Photo.Azure/AzureFileStorage.cs
@@ -0,0 +1,32 @@
+﻿using Photo.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.Azure
+{
+    public class AzureFileStorage : ICloudFileStorage
+    {
+        public Task<string> CompletedFileUrl(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> DownloadUserSubmittedFile(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UploadCompletedFile(Guid id, byte[] v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadUserSubmittedFile(Guid guid, byte[] image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Photo.UnitTest/Azure/AzureFileStorageFixture.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Photo.Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.UnitTest.Azure
+{
+    [TestFixture]
+    public class AzureFileStorageFixture
+    {
+        [Test]
+        public async Task CompletedFileUrl_RandomGuidReturnsNull()
+        {
+            var fileStorage = new AzureFileStorage();
+            var url = await fileStorage.CompletedFileUrl(Guid.NewGuid());
+            Assert.IsNull(url);
+        }
+
+        [Test]
+        public async Task DownloadUserSubmittedFile()
+        {
+            var id = Guid.NewGuid();
+
+            var fileStorage = new AzureFileStorage();
+
+            var expected = PhotoHelper.GetImageData();
+            await fileStorage.UploadUserSubmittedFile(id, expected);
+
+            var actual = await fileStorage.DownloadUserSubmittedFile(id);
+
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+
+        [Test]
+        public async Task UploadCompletedFile_ReturnsUrl()
+        {
+            var id = Guid.NewGuid();
+
+            var fileStorage = new AzureFileStorage();
+            var url = await fileStorage.UploadCompletedFile(id, PhotoHelper.GetImageData());
+
+            Assert.IsNotNull(url);
+        }    
+    }
+}
