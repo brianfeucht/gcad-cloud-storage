432cf38e250e8113dbfe256e4f61464c0fb76022
Brian Feucht <brian@litmus.com>
2015-06-10 22:19:11 -0700

    Test Fixture for AzureTable - Tests Failing

---
+++ Photo.Azure/AzureTable.cs
@@ -0,0 +1,28 @@
+﻿using Photo.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photo.Core.Models;
+
+namespace Photo.Azure
+{
+    public class AzureTable : ICloudTable
+    {
+        public Task<Guid> Add(CompletedMeme value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CompletedMeme> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CompletedMeme>> Latest(int skip = 0, int limit = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Photo.UnitTest/Azure/AzureTableFixture.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Photo.Azure;
+using Photo.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.UnitTest.Azure
+{
+    [TestFixture]
+    public class AzureTableFixture
+    {
+        [Test]
+        public async Task AddGetFind_GoldenPath()
+        {
+            var localTable = new AzureTable();
+
+            var expected = new CompletedMeme()
+            {
+                CreatedOn = DateTime.UtcNow,
+                Text = "UnitTest",
+                ImageUrl = "http://test.test/test.png"
+            };
+
+            var id = await localTable.Add(expected);
+
+            expected.Id = id;
+
+            var actual = await localTable.Get(id);
+
+            var printer = Utilities.CreatePrinter();
+            printer.Assert.PrintEquals(printer.PrintObject(expected), actual);
+
+            var collection = await localTable.Latest();
+
+            Assert.That(collection.Any(m => m.Id == id));
+        }
+    }
+}
