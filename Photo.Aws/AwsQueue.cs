82817f161ab6e0152cc7e93118917f1fb50d0c71
Brian Feucht <brian@litmus.com>
2015-06-11 21:33:59 -0700

    Fixture for AwsQueue

---
+++ Photo.Aws/AwsQueue.cs
@@ -0,0 +1,31 @@
+﻿using Photo.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photo.Core.Models;
+
+namespace Photo.Aws
+{
+    public class AwsQueue : ICloudQueue
+    {
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Task DequeueMessage(Func<MemeRequest, Task> processRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EnqueueMessage(MemeRequest message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Photo.UnitTest/Aws/AwsQueueFixture.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Photo.Aws;
+using Photo.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.UnitTest.Aws
+{
+    [TestFixture]
+    public class AwsQueueFixture
+    {
+        [Test]
+        public async Task Enqueue_EnqueuesMessage()
+        {
+            var localQueue = new AwsQueue();
+            await localQueue.EnqueueMessage(new MemeRequest());
+
+            Assert.AreEqual(1, localQueue.Count);
+        }
+
+        [Test]
+        public async Task Enqueue_DequeuesMessage()
+        {
+            var localQueue = new AwsQueue();
+            var expected = new MemeRequest();
+            await localQueue.EnqueueMessage(expected);
+            await localQueue.EnqueueMessage(new MemeRequest());
+
+            MemeRequest actual = null;
+            await localQueue.DequeueMessage(m => Task.Run(() => actual = m));
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(1, localQueue.Count);
+        }
+    }
+}
